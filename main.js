01e4a93174a06e324a9b060a0654696f3c104699
akaio <Zezinhotwo@users.noreply.github.com.>
2024-12-02 11:09:27 -0300

    adicionado LinkedList com Class em JS

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 import linkedList from "./linkedList.js";
+import LinkedList from "./LinkedList.js";
+
+console.log("FACTORY")
+console.log()
 
 const list = linkedList()
 
@@ -8,4 +12,19 @@ list.append("parrot");
 list.append("hamster");
 list.append("snake");
 list.append("turtle");
+console.log(list.toString())
+
+console.log()
+
+console.log("CLASS")
+
+// Class
+const list2 = new LinkedList();
+
+list2.append("dog");
+list2.append("cat");
+list2.append("parrot");
+list2.append("hamster");
+list2.append("snake");
+list2.append("turtle");
 console.log(list.toString())
